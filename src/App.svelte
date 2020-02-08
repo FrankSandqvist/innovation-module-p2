cb402a620717f8009863fff6dcc7b57c8b2df3ae
Frank Sandqvist <kankki@gmail.com>
2020-02-08 18:26:48 +0200

    ...

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,22 @@
 <script>
-	import Button from "./Button.svelte";
+  import Button from "./Button.svelte";
+  import TakePhoto from "./TakePhoto.svelte";
 </script>
 
 <style>
-  main {
+  body {
     font-family: sans-serif;
-    text-align: center;
+  }
+  .wrapper {
+    width: 30rem;
+    margin-left: auto;
+    margin-right: auto;
   }
 </style>
 
-<main>
-	<h1>Hello CodeSandbox</h1>
-	<h2>Start editing to see some magic happen!</h2>
-	<Button />
+<main class="wrapper">
+  <TakePhoto />
+  <h1>Hello CodeSandbox</h1>
+  <h2>Start editing to see some magic happen!</h2>
+  <Button />
 </main>
---
+++ src/TakePhoto.svelte
@@ -0,0 +1,32 @@
+<script>
+  let video = null;
+  let canvas = null;
+
+  if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then(function(s) {
+        video.srcObject = s;
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  }
+
+  function snap() {
+    canvas.getContext("2d").drawImage(video, 0, 0, 300, 200);
+  }
+</script>
+
+<style>
+  .video {
+    display: none;
+  }
+  .photo {
+    background-color: #666;
+  }
+</style>
+
+<button on:click={snap}>Test</button>
+<video class="video" autoplay bind:this={video} />
+<canvas class="photo" width="300px" height="200px" bind:this={canvas} />
